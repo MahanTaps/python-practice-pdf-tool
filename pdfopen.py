6e2fdefb7bfe982d9bfcc671252cdb508e715c7a
MahanTaps <tapiwat643@gmail.com>
2025-03-21 16:59:10 +0200

    feat: Add function to extract text from the first page of a PDF

--- pdfopen.py
+++ pdfopen.py
@@ -3,8 +3,10 @@ import pypdf
 def verify_pdf(document):
     return 'PDF'in magic.from_file(document)
 def first_page_text(document):
-    #creating a pdf reader object 
-    return None
+    from pypdf import PdfReader
+    reader= PdfReader(document)
+    page=reader.pages[0]
+    print (page.extract_text())
 
 if __name__=="__main__":
     #takes the example file
@@ -14,6 +16,7 @@ if __name__=="__main__":
         try:
             with open(filename,'r') as file_example:
                 print("File opened!")
+            first_page_text(filename)
         except Exception as e: 
             print("e")
     else: 
