1e8a8efdf0b023353c43083f87f09596cecf23c5
MahanTaps <tapiwat643@gmail.com>
2025-03-21 17:14:20 +0200

    test: Add pytest test for first_page_text function

--- test_pdfopen.py
+++ test_pdfopen.py
@@ -1,6 +1,15 @@
 from pdfopen import verify_pdf
-
+from pdfopen import first_page_text
+from io import StringIO
+import sys
 def test_verify_pdf():
     assert verify_pdf("test.pdf")==True, "This is a PDF so it should be true"
     assert verify_pdf("example.pdf")==False, "This is a PDF so it should be true"
     assert verify_pdf("README.txt")==True, "This is not a pdf, so it should be false"
+def test_first_page_text():
+    sys.stdout=StringIO()
+    first_page_text("example.pdf")
+    output=sys.stdout.getvalue().strip()
+    sys.stdout=sys.__stdout__
+
+    assert (len(output)>0)==True, "Text should be scraped from the PDF, so it should be true"
