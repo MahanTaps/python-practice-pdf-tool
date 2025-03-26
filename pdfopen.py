5d69e9682b62755d4c18a13077c38f58d57c5cdd
MahanTaps <tapiwat643@gmail.com>
2025-03-26 10:36:33 +0200

    refactor: Remove unnecessary open() call and comments

--- pdfopen.py
+++ pdfopen.py
@@ -11,17 +11,12 @@ def first_page_text(document):
     print (page.extract_text())
 
 if __name__=="__main__":
-    #takes the example file
     filename="example.pdf" 
-    #checks if it is a PDF
     if verify_pdf(filename):
         try:
-            with open(filename,'r') as file_example:
-                print("File opened!")
             first_page_text(filename)
         except Exception as e: 
-            print("e")
+            print("Exception",e)
     else: 
         print("File is not a PDF!")
 
-    #Checking the file type
